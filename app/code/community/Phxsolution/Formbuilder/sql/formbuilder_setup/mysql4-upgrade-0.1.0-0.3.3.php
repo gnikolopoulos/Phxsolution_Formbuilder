5fcd7f3c37db09eded247c8e21054b466742e8e6
George Nikolopoulos <georgen@interactive-design.gr>
2020-02-05 10:18:16 +0200

    Fixed: Field is added to the wrong table

--- app/code/community/Phxsolution/Formbuilder/sql/formbuilder_setup/mysql4-upgrade-0.1.0-0.3.3.php
+++ app/code/community/Phxsolution/Formbuilder/sql/formbuilder_setup/mysql4-upgrade-0.1.0-0.3.3.php
@@ -3,5 +3,5 @@ $installer = $this;
 $installer->startSetup();
 $installer->run("
 ALTER TABLE {$this->getTable('formbuilder_forms')} CHANGE `title` `form_title` VARCHAR(255) NOT NULL; 
-ALTER TABLE {$this->getTable('formbuilder_forms')} ADD `cssclass` VARCHAR(255) NOT NULL;");
+ALTER TABLE {$this->getTable('formbuilder_fields')} ADD `cssclass` VARCHAR(255) NOT NULL;");
 $installer->endSetup();
